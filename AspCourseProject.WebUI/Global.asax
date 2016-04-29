cb47cd5f8cea120de2c3039ef117ce4b4c815b75
Dodo888 <darkdodo888@gmail.com>
2016-04-29 10:58:31 +0500

    Project Layout

---
+++ AspCourseProject.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspCourseProject.WebUI.MvcApplication" Language="C#" %>
---
+++ AspCourseProject.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspCourseProject.WebUI.Startup))]
+namespace AspCourseProject.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
