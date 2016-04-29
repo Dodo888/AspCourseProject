d5a84e0c63b9e898fd5ad39a9ae0d1682708b8a5
Dodo888 <darkdodo888@gmail.com>
2016-04-29 11:08:14 +0500

    Person Model

---
+++ AspCourseProject.Domain/Entities/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspCourseProject.Domain.Entities
+{
+    class Person
+    {
+        public int PersonId { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public int Rating { get; set; }
+        public int Price { get; set; }
+        public string Category { get; set; }
+        public bool Gender { get; set; }
+        public bool Status { get; set; }
+    }
+}
